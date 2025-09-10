9ea8bda063c62aec06b727cb703e5e96c3e6f0cb
Marinho777x <lucas.julio.ferreira@escola.pr.gov.br>
2025-09-10 12:30:43 +0000

    fizemos 9 cartoes no js

---
+++ perguntas.js
@@ -0,0 +1,50 @@
+criaCartao(
+    'programaçaõ,'
+    'o que é html',
+    resposta
+)
+criaCartao(
+    'programaçaõ,'
+    'o que é css'
+    resposta
+)
+
+criaCartao(
+    'programaçaõ,'
+    'o que é js'
+    resposta
+)
+
+criaCartao(
+    'programaçaõ,'
+   
+    resposta
+)
+criaCartao(
+    'programaçaõ,'
+  
+    resposta
+)
+
+criaCartao(
+    'programaçaõ,'
+    
+    resposta
+)
+
+criaCartao(
+    'programaçaõ,'
+    
+    resposta
+)
+criaCartao(
+    'programaçaõ,'
+   
+    resposta
+)
+
+criaCartao(
+    'programaçaõ,'
+    
+    resposta
+)
